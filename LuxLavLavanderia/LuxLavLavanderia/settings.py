b11ec051298db10787a15d7ae8461b64b236fa71
Pedro Igor <pedro.igor.pitl@gmail.com>
2024-11-13 17:16:05 -0300

    Conexão com o PostGreSQL

--- LuxLavLavanderia/LuxLavLavanderia/settings.py
+++ LuxLavLavanderia/LuxLavLavanderia/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'LuxLavLavanderia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LuxLavLavanderia',
+        'USER': 'postgres',
+        'PASSWORD': 'pedroBD',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
 
